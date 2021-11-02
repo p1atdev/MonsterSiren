8f92d3ac8399f60df25636a0f160986f5442e622
Shu Teiei <shu_teiei@outlook.jp>
2021-11-02 14:30:59 +0900

    Initial Commit

---
+++ MonsterSiren/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MonsterSiren
+//
+//  Created by p1atdev on 2021/11/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
